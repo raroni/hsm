a4f823a643ba0b2f768700c3fb8792aadbdfa9ee
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2014-04-28 15:43:36 +0200

    Transition now takes a target state in constructor.

--- lib/src/transition.dart
+++ lib/src/transition.dart
@@ -3,4 +3,6 @@ part of hsm;
 abstract class Transition {
   State target;
   bool get isSatisfied;
+  
+  Transition(State this.target);
 }
--- test/mocks/transition_mock.dart
+++ test/mocks/transition_mock.dart
@@ -3,9 +3,7 @@ part of mocks;
 class TransitionMock extends Transition {
   bool nextIsSatisfied;
   
-  TransitionMock(State target, bool this.nextIsSatisfied) {
-    this.target = target; 
-  }
+  TransitionMock(State target, bool this.nextIsSatisfied) : super(target);
   
   bool get isSatisfied => nextIsSatisfied;
 }
